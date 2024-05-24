619344d8e07a6a29dd39ce6d7da5e907f9162eb5
Hitkumar <hit98987@gmail.com>
2024-05-24 19:05:11 -0400

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      gray: "#d9d9d9",
+      purple: "#a57dfc",
+      red: "#ee5a5a",
+      black: "#000000",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
